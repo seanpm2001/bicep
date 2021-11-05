a662cb6656c5cbe2c3d2a81772b8eb74f1a2bbf1
Anthony Martin <antmarti@microsoft.com>
2021-11-05 14:21:33 -0400

    WIP: convert expression generation logic to use IR

---
+++ src/Bicep.Core/CodeAnalysis/ModuleReferenceOperation.cs
@@ -0,0 +1,21 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+using Bicep.Core.Semantics;
+
+namespace Bicep.Core.CodeAnalysis
+{
+    public class ModuleReferenceOperation : Operation
+    {
+        public ModuleReferenceOperation(ModuleSymbol symbol, IndexReplacementContext? indexContext)
+        {
+            Symbol = symbol;
+        }
+
+        public ModuleSymbol Symbol { get; }
+
+        public IndexReplacementContext? IndexContext { get; }
+
+        public override void Accept(IOperationVisitor visitor)
+            => visitor.VisitModuleReferenceOperation(this);
+    }
+}
